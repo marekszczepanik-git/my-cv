c27f5aeaf36c14306fa2d3cc2849bfcc63da5fc1
marekszczepanik-git <github.markus@gmail.com>
2025-05-24 18:00:00 +0200

    Create data structure

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,34 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
+interface Experience {
+  year: number;
+  description: string;
+}
+interface CVData {
+  photo: string;
+  name: string;
+  lastName: string;
+  position: string,
+  experience: Experience[];
+  education: string[];
+}
 
+const cvData: CVData = {
+  photo: 'https://placehold.co/600x400?text=MS',
+  name: 'Marek',
+  lastName: 'Szczepanik',
+  position: 'Developer',
+  experience: [
+    { year: 2003, description: 'Lorem ipsum' },
+    { year: 2025, description: 'Dolor sit amet'}
+  ],
+  education: [
+      'Lorem ipsum',
+      'Dolor sit amet',
+      'Ala ma kota'
+  ]
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
