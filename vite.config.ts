0255085e63b6a8f28ff7e5ce91ff5083fe052108
marekszczepanik-git <github.markus@gmail.com>
2025-05-24 14:22:27 +0200

    Create vite config file

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: 3000, // port serwera
+  },
+  build: {
+    outDir: "build", // wyjściowy katalog dla projektu
+  },
+  css: {
+    devSourcemap: true, // ustawia mapy źródłowe dla CSS
+  },
+});
